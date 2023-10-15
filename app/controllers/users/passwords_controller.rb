e09da9b371eff47b0eeb244a6fff82639980d456
EC2 Default User <ec2-user@ip-172-31-23-21.ap-northeast-1.compute.internal>
2023-10-15 19:33:17 +0900

    passwordコントローラファイルの修正

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -2,6 +2,11 @@
 
 class Users::PasswordsController < Devise::PasswordsController
   before_action :check_guest, only: :create
+
+  def check_guest
+    if params[:user][:email].downcase == 'guest@example.com'
+      redirect_to root_path, alert: "ゲストユーザーの編集・削除はできません。"
+    end
   end
   # GET /resource/password/new
   # def new
