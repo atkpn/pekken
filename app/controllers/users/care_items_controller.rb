02ddf3a0e1c502b5b54ea915e7859bb86316b02d
EC2 Default User <ec2-user@ip-172-31-23-21.ap-northeast-1.compute.internal>
2023-10-17 20:08:30 +0900

    [update]自分のテンプレート追加時のバリデーション

--- app/controllers/users/care_items_controller.rb
+++ app/controllers/users/care_items_controller.rb
@@ -1,17 +1,16 @@
 class Users::CareItemsController < ApplicationController
 
   def create
-    care_item = CareItem.new(care_item_params)
-    care_item.user_id = current_user.id
-    # if
-      care_item.save
+    @care_item = CareItem.new(care_item_params)
+    @care_item.user_id = current_user.id
+    if @care_item.save
       redirect_to request.referer #更新後もリダイレクト先のページは特定のペットの項目追加画面のまま
-    # else
-      # @pet = Pet.find(params[:pet_id])
-      # @default_items = CareItem.where(default_item: true)
-      # @custom_items = CareItem.where(default_item: false, user_id: current_user.id)
-      # render "users/cares/new"
-    # end
+    else
+      @pet = Pet.find(params[:pet_id])
+      @default_items = CareItem.where(default_item: true)
+      @custom_items = CareItem.where(default_item: false, user_id: current_user.id)
+      render "users/cares/new"
+    end
   end
 
   def edit
